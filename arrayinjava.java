7880f183cfe94ea15283e29d5073caa7b4fa2707
NaveenKumarAmaresh <90778148+NaveenKumarAmaresh@users.noreply.github.com>
2021-11-16 20:45:58 +0530

    Add files via upload

---
+++ arrayinjava.java
@@ -0,0 +1,13 @@
+package navven;
+
+public class arrayinjava {
+	 public static void main(String[] args) {
+	        int[] a = new int[]{10, 20, 70, 40, 50};
+
+	        for(int i = 0; i < a.length; ++i) {
+	            System.out.println(a[i]);
+	        }
+
+	    }
+	}
+
